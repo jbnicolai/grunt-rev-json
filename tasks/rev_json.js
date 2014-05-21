e84077adbd1d7df35a195317b2b23c751b154360
Xiaoyi <ashi009@gmail.com>
2014-05-22 03:34:36 +0800

    Initilize

---
+++ tasks/rev_json.js
@@ -0,0 +1,76 @@
+var pth = require('path');
+var chalk = require('chalk');
+var crypto = require('crypto');
+var crc32c = require('fast-crc32c');
+
+console.log(crc32c);
+
+var kAlgorithms = {
+  crc32: function(buf) {
+    var checksum = crc32c.calculate(buf);
+    var buf = new Buffer(4);
+    buf.writeUInt32BE(checksum, 0);
+    return buf;
+  },
+  default: function(algorithm) {
+    try {
+      crypto.createHash(algorithm);
+    } catch(e) {
+      return null;
+    }
+    return function(buf) {
+      return crypto.createHash(algorithm).update(buf).digest();
+    };
+  }
+};
+
+function toUrlSafeBase64(base64) {
+  return base64.replace(/[+\/]/g, function(ch) {
+    return ch == '+' ? '-' : '_';
+  }).replace(/=+$/, '');
+}
+
+function relativePath(from, to) {
+  var fromDir = pth.dirname(from);
+  var toDir = pth.dirname(to);
+  var res = pth.basename(to);
+  if (fromDir !== toDir)
+    res = pth.relative(fromDir, toDir) + pth.sep + res;
+  return res;
+}
+
+module.exports = function(grunt) {
+
+grunt.registerMultiTask('rev_json', 'Digest files and outpu as a json file.', function() {
+
+  var options = this.options({
+    algorithm: 'crc32',
+    encoding: 'base64',
+    urlSafe: true,
+    length: 8
+  });
+
+  var hasher = options.algorithm;
+  if (typeof hasher == 'string')
+    hasher = kAlgorithms[hasher] || kAlgorithms.default(hasher);
+  if (!hasher)
+    return grunt.fail('Cannot find algorithm "' + options.algorithm + '".');
+
+  this.files.forEach(function(file) {
+    var versions = {};
+    file.src.forEach(function(path) {
+      var version = hasher(grunt.file.read(path)).toString(options.encoding);
+      if (options.encoding == 'base64' && options.urlSafe)
+        version = toUrlSafeBase64(version);
+      version = version.substr(0, options.length);
+      grunt.log.writeln('File ' + chalk.cyan(path) + ' hashed (' +
+          chalk.cyan(version) + ').');
+      versions[relativePath(file.dest, path)] = version;
+    });
+    grunt.file.write(file.dest, JSON.stringify(versions));
+    grunt.log.writeln('File ' + chalk.cyan(file.dest) + ' created (rev).')
+  });
+
+});
+
+};
